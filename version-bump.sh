20589511d3664214d4c4abb413219cec1583cade
Giles Westwood <giles.westwood@gmail.com>
2023-04-24 13:09:30 +0100

    refactor to make patch bump default

--- version-bump.sh
+++ version-bump.sh
@@ -17,7 +17,7 @@ prefix='v'
 do_tag=0
 tag=${GHA_TAG:-latest}
 USER=$(whoami)
-bump=${GHA_BUMP:-gitlog}
+bump=${GHA_BUMP:-patch}
 major=0
 minor=0
 patch=0
@@ -34,20 +34,28 @@ I_USAGE="
   Usage:  ${NAME} [OPTIONS]
 
   Description:
-    Lookup the latest git tag in the current repo and based on the bump value increase it
-    Set this as a github output to use in an action
-    If a tag is provided just return that tag as an output. (This allows us to have a standard github variable name)
+    Lookup the latest semver tag of a git checkout and optionally bump it
+
+    Bump calculation, default is patch but the recent git logs are checked for #minor #major commits to override this.
+    Also the git branch name is checked for feat/ or fix/ equivilants to get a bump that is higher than patch.
+
+    In some cases we may want to keep the value so we use a bump of 'none' . In some cases we might want to specify a version as well
+    rather than using git tags or any bumping, so setting a specific tag and not 'latest' will just return that value.
 
   Github actions inputs:
 
     To handle running this script via github actions where it can be called from a dispatch, or a commit trigger
     The version is fed in through an environment variable, GHA_TAG which can be optional (unlike an argument to the script)
 
+  Requirements:
+
+    This script needs to be run in a git checkout directory. For github actions this is done using the actions/checkout step before
+
   Options:
       -p | --prefix       ) set the version prefix ($prefix) and search based on this
       -d | --do_tag       ) perform tagging of the repo
       -t | --tag          ) provide a tag
-      -b | --bump         ) bump either patch|minor|major or ($bump) using the a #minor #major git commit message default patch
+      -b | --bump         ) bump either none patch|minor|major or ($bump) using the a #minor #major git commit message default patch
 "
   echo "$I_USAGE"
   exit
@@ -85,7 +93,7 @@ GITHUB_OUTPUT=${GITHUB_OUTPUT:-/tmp/$NAME.$USER}
 #set -x
 
 # option that just returns the provided values
-if [[ "$bump" = 'gitlog' ]] && [[ "$tag" != 'latest' ]];then
+if [[ "$tag" != 'latest' ]];then
   echo "tag specified: $tag"
   echo "version_tag=${tag}" >> "$GITHUB_OUTPUT" 2>/dev/null
   tag_numeric="$(echo $tag | tr -dc '[:digit:].')"
@@ -108,44 +116,6 @@ vnum2=${version_bits[1]}
 vnum3=${version_bits[2]}
 vnum1=`echo $vnum1 | sed 's/v//'`
 
-# Allow git commit messages to override bump value
-# Check for #major or #minor in commit message and increment the relevant version number
-if [[ "$bump" = 'gitlog' ]];then
-
-  latest_log=$(git log --format=%B -n 1 HEAD)
-  merge_commit=$(echo "$latest_log" | head -n1)
-
-  echo "latest_log:"
-  echo "$latest_log"
-
-  echo "merge_commit:"
-  echo "$merge_commit"
-
-  if grep -qE 'feat' <<< $(echo $merge_commit);then
-    echo "feature git commit detected"
-    minor=1
-  fi
-
-  if grep -qE 'fix|bug|patch|test' <<< $(echo $merge_commit);then
-    echo "fix|bug|patch|test git commit detected"
-    patch=1
-  fi
-
-  if grep -q '#major' <<< $(echo $latest_log) ;then
-    echo "major git commit detected"
-    major=1
-  fi
-  if grep -q '#minor' <<< $(echo $latest_log) ;then
-    echo "minor git commit detected"
-    minor=1
-  fi
-  if grep -q '#patch' <<< $(echo $latest_log) ;then
-    echo "patch git commit detected"
-    patch=1
-  fi
-
-fi
-
 # take bumping from arguments
 
 if [[ "$bump" = 'major' ]];then
@@ -158,6 +128,40 @@ if [[ "$bump" = 'patch' ]];then
   patch=1
 fi
 
+# Allow git commit messages to override bump value
+# Check for #major or #minor in commit message and increment the relevant version number
+
+latest_log=$(git log --format=%B -n 1 HEAD)
+merge_commit=$(echo "$latest_log" | head -n1)
+
+echo "latest_log:"
+echo "$latest_log"
+
+echo "merge_commit:"
+echo "$merge_commit"
+
+if grep -qE 'feat' <<< $(echo $merge_commit);then
+  echo "feature git commit detected"
+  minor=1
+fi
+
+if grep -qE 'fix|bug|patch|test' <<< $(echo $merge_commit);then
+  echo "fix|bug|patch|test git commit detected"
+  patch=1
+fi
+
+if grep -q '#major' <<< $(echo $latest_log) ;then
+  echo "major git commit detected"
+  major=1
+fi
+if grep -q '#minor' <<< $(echo $latest_log) ;then
+  echo "minor git commit detected"
+  minor=1
+fi
+if grep -q '#patch' <<< $(echo $latest_log) ;then
+  echo "patch git commit detected"
+  patch=1
+fi
 
 
 
@@ -165,18 +169,22 @@ fi
 # perform version bumping
 #
 
-if [[ "$major" -eq 1 ]]; then
-    echo "Update major version"
-    vnum1=$((vnum1+1))
-    vnum2=0
-    vnum3=0
-elif [[ "$minor" -eq 1 ]]; then
-    echo "Update minor version"
-    vnum2=$((vnum2+1))
-    vnum3=0
-elif [[ "$patch" -eq 1 ]]; then
-    echo "Update patch version"
-    vnum3=$((vnum3+1))
+if [[ "$bump" != 'none' ]];then
+  echo "bump !=none so performing version bump"
+
+  if [[ "$major" -eq 1 ]]; then
+      echo "Update major version"
+      vnum1=$((vnum1+1))
+      vnum2=0
+      vnum3=0
+  elif [[ "$minor" -eq 1 ]]; then
+      echo "Update minor version"
+      vnum2=$((vnum2+1))
+      vnum3=0
+  elif [[ "$patch" -eq 1 ]]; then
+      echo "Update patch version"
+      vnum3=$((vnum3+1))
+  fi
 fi
 
 # create new tag
